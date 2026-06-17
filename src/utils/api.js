615fe558a00ed7fe40218747c1badb5c62b661df
Karthik Guttula <karthikguttula107@gmail.com>
2026-06-17 10:29:18 +0200

    Confifure Vite API proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://10.62.21.3:5000/api",
+  baseURL: "/api",
 });
 
 api.interceptors.request.use((config) => {
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react],
 
   server: {
-    host: "0.0.0.0",
+    host: true,
 
     proxy: {
       "/api": {
